58ae700f95042660c04bba55a3b65d52add0e097
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-04-03 16:52:08 -0400

    Update select.sql

--- select.sql
+++ select.sql
@@ -6,3 +6,18 @@ SELECT * FROM Officers;
 
 SELECT * FROM Crime_charges cch, Crime_officers o, Crime_codes cc
 WHERE cch.Crime_ID == o.Crime_ID AND cch.Crime_code == cc.Crime_code;
+
+SELECT *
+FROM Alias;
+
+SELECT *
+FROM Criminals;
+
+SELECT *
+FROM Crimes;
+
+SELECT *
+FROM Sentences;
+
+SELECT *
+FROM Prob_officer;
