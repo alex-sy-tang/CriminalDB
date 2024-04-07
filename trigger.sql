cf8ff970301ae7681db46947884f557b0f8431e4
alex-sy-tang <57281358+alex-sy-tang@users.noreply.github.com>
2024-04-07 13:28:34 -0400

    Created triggers for database

---
+++ trigger.sql
@@ -0,0 +1,35 @@
+-- Update the number of violation for a criminal, 
+-- when a new sentencing is initiated
+
+DELIMITER @@
+CREATE OR REPLACE TRIGGER updateViolations
+AFTER INSERT ON Sentencing 
+FOR EACH ROW
+    BEGIN 
+        IF NEW.Sentencing_ID IS NOT NULL THEN
+            UPDATE Sentencing
+            SET Number_of_violations = Number_of_violations + 1
+            WHERE Criminal_ID = NEW.Criminal_ID;
+        END IF;
+    END; 
+END @@
+DELIMITER; 
+
+
+
+
+-- Change the fine amount and court fee when the Charge_Status changed
+DELIMITER @@
+CREATE OR REPLACE TRIGGER updateFine
+AFTER UPDATE ON Crime_charges
+FOR EACH ROW 
+    BEGIN
+        UPDATE Crime_charges
+        SET Fine_amount = 50000
+        WHERE Charge_Status = 'GL'; 
+
+        UPDATE Crime_charges
+        SET Court_fee = 20000
+        WHERE Charge_status = 'GL'; 
+    END @@
+DELIMITER; 
