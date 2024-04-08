abb206b9d6d50bc1d8100ae82051667b047a09d1
JaxsinL <165234308+JaxsinL@users.noreply.github.com>
2024-04-07 21:24:18 -0400

    Update select.sql

--- select.sql
+++ select.sql
@@ -4,11 +4,8 @@ SELECT * FROM Appeals;
 
 SELECT * FROM Officers;
 
-SELECT * FROM Crime_charges cch, Crime_officers o, Crime_codes cc
-WHERE cch.Crime_ID == o.Crime_ID AND cch.Crime_code == cc.Crime_code;
-
 SELECT *
-FROM Alias;
+FROM Aliases;
 
 SELECT *
 FROM Criminals;
@@ -17,7 +14,15 @@ SELECT *
 FROM Crimes;
 
 SELECT *
-FROM Sentences;
+FROM Sentencing;
 
 SELECT *
 FROM Prob_officer;
+
+SELECT codes.Code_description
+FROM Crime_codes codes
+WHERE codes.Crime_code IN (SELECT cc.Crime_code
+                           FROM Crime_charges cc, Crimes cr, Criminals cal
+                           WHERE cc.Crimes_ID = cr.Crimes_ID AND cal.Criminal_ID = cr.Criminal_ID);
+
+
