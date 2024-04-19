26ac7b76e927609aedfa2e1fb14e0431ef608ee0
lcm9995 <112994511+lcm9995@users.noreply.github.com>
2024-04-19 19:44:03 -0400

    Update crime_charges.php

--- db_site/table_pages/crime_charges.php
+++ db_site/table_pages/crime_charges.php
@@ -23,7 +23,7 @@ $mysqli->close();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel = "stylesheet" href = "styles/style.css">
+	<link rel = "stylesheet" href = "../styles/style.css">
 	
 </head>
 <body>
