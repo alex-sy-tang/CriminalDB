9a577f54200e0d3bb7105d996ae27a08848bef1e
alex-sy-tang <57281358+alex-sy-tang@users.noreply.github.com>
2024-04-21 13:56:32 -0400

    Add files via upload

--- db_site/table_pages/officers.php
+++ db_site/table_pages/officers.php
@@ -1,23 +1,25 @@
-<?php
+<?php 
+
 session_start();
 include "../connect.php";
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
   // User is not logged in, redirect to login page
-  header('Location: login.html');
+  header('Location: ../login.html');
   exit;
 }
 
-$sql = " SELECT * FROM Officers ";
-$result = $conn->query($sql);
-$conn->close();
-?>
+?> 
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel = "stylesheet" href = "../styles/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src = "	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 	
 </head>
 <body>
@@ -25,22 +27,26 @@ $conn->close();
     	<div class="logo">
       		<h1>CRIMINAL DATABASE</h1>
     	</div>
-    
+	
     	<div class="search-bar">
-      		<form action="#">
-        		<input type="text" placeholder="Search">
+			<form action="officers.php" method = "GET">
+        		<input type="text" name = "search_by_id" placeholder="Enter ID">
+				<button type = "submit">Search</button>
       		</form>
     	</div>
     	<ul>
-			<li><a href="../logout.php" class="login">Logout</a></li>
-	</ul>
+   			<li><a href="../login.html" class="login">Logout</a ></li>
+     	</ul>
+		<ul>
+			<li><a href="../table_pages/officers.php" class="login">Back</a ></li>
+		</ul>
 	 </nav>
-	<div class="table_content">
+	<div id="table_content" class = "container">
 		<div class="table header">
 			<h1>Officers</h1>
 		</div>
 		<div class = "table holder">
-			<table class="full_table">
+			<table class="table">
 				<tr>
 					<th>Officer ID</th>
 					<th>Last Name </th>
@@ -51,21 +57,68 @@ $conn->close();
 					<th>Status</th>
 				</tr>
 				<?php
-					while($rows=$result->fetch_assoc()){
+                    include_once "../connect.php"; 
+                    mysqli_query($conn, "LOCK TABLES Officers READ");
+                    // if(isset($_POST['search_by_id'])){
+                    //     $id = $_POST['get_id'];
+                    //     $query = "SELECT * FROM employee WHERE id='$id' "; 
 
-				?>
-				<tr>
-						<td><?php echo $rows['Officer_ID'];?></td>
-						<td><?php echo $rows['Last_name'];?></td>
-						<td><?php echo $rows['First_name'];?></td>
-						<td><?php echo $rows['Precinct'];?></td>
-						<td><?php echo $rows['Badge'];?></td>
-						<td><?php echo $rows['Phone'];?></td>
-						<td><?php echo $rows['Status'];?></td>
-				</tr>
-				<?php
-					}
-				?>
+                    // }
+                   
+
+                    $sql = "SELECT * FROM Officers";
+                    $params = [];
+                    $types = '';
+
+                    if($_SERVER["REQUEST_METHOD"] == "GET"){
+                        if(!empty($_GET["search_by_id"])){
+                            $sql .= " WHERE Officer_ID = ?";
+                            $params[] = $_GET['search_by_id'];
+                            $types .= 'i'; 
+                        }
+                    }
+                    
+
+                    $stmt = $conn -> prepare($sql);
+
+                    if($params){
+                        $stmt -> bind_param($types, ...$params); 
+                    }
+
+                    $stmt -> execute();
+                    $result = $stmt -> get_result(); 
+                    // if(isset($_POST['search'])){
+                    //     $id = $_POST['search_by_id']; 
+                    //     $sql = " SELECT * FROM Aliases WHERE Alias_ID = $id ";  
+                    // }
+
+
+                    //$result = $conn -> query($sql); 
+
+                    if(!$result){
+                        die("Invalid query: " . $conn -> error); 
+                    }
+
+                    while($row = $result -> fetch_assoc()){
+                        echo"
+                        <tr>
+                            <td>$row[Officer_ID]</td>
+                            <td>$row[Last_name]</td>
+                            <td>$row[First_name]</td>
+							<td>$row[Precinct]</td>
+                            <td>$row[Badge]</td>
+                            <td>$row[Phone]</td>
+							<td>$row[Status_]</td>
+                           
+                        </tr>
+                        ";
+                    }
+
+                    mysql_query($conn, "UNLOCK TABLES");
+					$conn->close();
+					
+
+                    ?>
 			</table>
 		</div>
 	</div>
