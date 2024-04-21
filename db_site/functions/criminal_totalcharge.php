210f9e2532507e597a66e29938f47b23fe06667d
JaxsinL <165234308+JaxsinL@users.noreply.github.com>
2024-04-21 01:04:20 -0400

    Update criminal_totalcharge.php

--- db_site/functions/criminal_totalcharge.php
+++ db_site/functions/criminal_totalcharge.php
@@ -4,7 +4,7 @@ include_once "../connect.php";
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
   // User is not logged in, redirect to login page
-  header('Location: login.html');
+  header('Location: ../login.html');
   exit;
 }
 
